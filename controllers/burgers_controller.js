e0b2c06a74993d4147142e91daa74d70220cc61d
cslane64 <cslane@gmail.com>
2019-06-05 15:08:03 -0400

    ready for logic - ugh

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,49 @@
+var express = require("express");
+
+var router = express.Router();
+
+var cat = require("../models/cat.js");
+
+//selectAll()
+router.get("/", function(req, res) {
+    /*cat.all(function(data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });*/
+  });
+
+  //insertOne
+  router.post("/api/cats", function(req, res) {
+    /*cat.create([
+      "name", "sleepy"
+    ], [
+      req.body.name, req.body.sleepy
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });*/
+  });
+  //updateOne
+  router.put("/api/cats/:id", function(req, res) {
+    /*var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    cat.update({
+      sleepy: req.body.sleepy
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });*/
+  });
+
+
+
+module.exports = router;
