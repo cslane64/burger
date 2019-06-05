dcbedceceeec128897a12299a2214291e26077cf
cslane64 <cslane@gmail.com>
2019-06-04 20:08:38 -0400

    file structure set up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Steak burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers (name) VALUES ('Plain burger');
+INSERT INTO burgers (name) VALUES ('Beef Brisket Burger');
+INSERT INTO burgers (name) VALUES ('Pulled Pork Burger');
